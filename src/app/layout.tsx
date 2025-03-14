78bfe7b677fd6f754548bbb67779334c92bfc0af
ONEONUORA <monweebube44@gmail.com>
2025-03-14 01:09:31 +0100

    Dapp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Providers } from "@/components/Providers";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Starknet Basecamp XI",
-  description: "Counter App",
+  title: "FortiChain",
+  description: "Fortichain App",
 };
 
 export default function RootLayout({
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+       {children}
       </body>
     </html>
   );
